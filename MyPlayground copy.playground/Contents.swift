6e5867a4e02c76d1a8f9d0980fe810726232792a
bima takuya <bimatakuya81@gmail.com>
2024-02-25 14:07:40 +0900

    from bima

---
+++ MyPlayground copy.playground/Contents.swift
@@ -0,0 +1,193 @@
+//var nama : String = "Bima Takuya"
+//var umur : Int = 23
+//var tinggi : Double = 183.0
+//var menikah : Bool = false
+//
+//nama = "bambang"
+//
+//
+//
+//nama = "bima takuya"
+//print("nama saya \(nama) umurku \(umur) tinggi badan ku \(tinggi)setatus menikah \(menikah)")
+//
+//let isa = "isa nur fajar"
+//let zega = "iqbal zega"
+//let fathur = "fathur rahman"
+//let bima = "bima takuya"
+//
+//let ippp = (isa,zega,fathur,bima)
+//
+//
+//
+//import UIKit
+//
+//// ini adalah dictionary
+//var person: [String: String] = ["name": "isa", "age": "31", "status": "sudah menikah"]
+//
+//person.updateValue("bima", forKey: "nama")
+//print(person["nama"])
+//
+//
+//
+//var angkaPertama = 0
+//var angkaKedua = 200
+//
+//var hasil1 = angkaPertama + angkaKedua
+//var hasil2 = angkaPertama = angkaKedua
+//var hasil3 = angkaPertama / angkaKedua
+//var hasil4 = angkaPertama - angkaKedua
+//var hasil5 = angkaPertama % angkaKedua
+//
+//print(hasil1)
+//print(hasil2)
+//print(hasil3)
+//print(hasil4)
+//print(hasil5)
+//
+//
+//
+//
+//var a = 10
+//var b = 20
+//
+//var hasila = a + 10
+//var hasilb = b - 10
+//
+//print(hasila)
+//print(hasilb)
+//
+//var nama1 : String = "fatur"
+//var nama2 : String = "bima"
+//var nama3 : String = "isa"
+//var nama4 : String = "iqbal"
+//var nama5 : String = "bian"
+//var nama6 : String = "harto"
+//var mizonokuchi = [nama1, nama2, nama3, nama4, nama5]
+//
+//mizonokuchi [0]
+//mizonokuchi [1]
+//mizonokuchi [2]
+//mizonokuchi [3]
+//mizonokuchi [4]
+//
+//mizonokuchi.append(nama6)
+//
+
+
+//var a = 100
+//var b = 200
+//
+//a = a + b
+//b = a - b
+//a = a - b
+//
+//
+//let stringA: String = "asu"
+//let stringB: String = "Asu"
+///// arithmetic operator comparison
+//
+//
+
+let valueA: Int = 10
+let valueB: Int = 10
+//
+/// mark: comprasion operator
+// == sama dengan
+// != tidak sama dengan
+// > lebih besar dari
+// < lebih kecil dari
+// >= lebih besar ATAU sama dengan
+// <= lebih kecil ATAU sama dengan
+// || or atau (jika salah satu true hasilnya true) (kalau keduanya false masuk false)
+// && and (jika false salah satu maka hasilnya false) (kalau keduanya true masuk ke true)
+//
+//
+//if (valueA <= valueB) && (valueA >= valueB) {
+//    // true
+//    print("A")
+//} else {
+//    // false
+//    print("B")
+//}
+//
+//
+//let valueA: Int = 1922
+//let valueB: Int = 1922
+//
+//if valueA > valueB {
+//    // kondisi pertama
+//} else if valueA < valueB {
+//    // kondisi kedua
+//}
+
+
+//let count = 1...10
+//for number in count {
+//    print("Number is \(number)")
+//}
+//
+//let albums = ["red", "1989", "reputation"]
+//for album in albums {
+//    print("\(album) is on Apple Music")
+//}
+//
+//print("players gonna")
+//
+//for _ in 1...5 {
+//
+//[ kurung siku]
+//{kurung keurawal}
+//(kurung biasa)
+
+
+
+
+//
+//import UIKit
+//
+//var stringArrays: [String] = ["one", "two", "three", "four", "five"]
+//
+//for array in stringArrays {
+//    print("ini adalah \(array)")
+//}
+//
+//for index in 1...1000 {
+//    // perulangan ini akan mencetak angka dari 1 sampai 10000
+//    // dan mencetak apakah angka tersebut genap atau ganjil
+//    if index % 2 == 0{
+//        print("ini adalah bilangan genap \(index)")
+//    }else{
+//        print("ini bilangan ganjil \(index)")
+//    }
+//    
+//}
+//
+//
+//
+//var number: Int = 20
+//while number <= 50 {
+//    print("angka \(number)")
+//    number += 1
+//}
+//print(number)
+//
+//
+////repeat
+////
+////
+////bikin ganjil genap sampai 1000 pakai while
+////prepare fungsion
+////    instal sourcetree
+////    
+    
+var number: Int = 0
+
+while number <= 10000 {
+    if number % 2 == 0 {
+        print ("\(number) genap")
+    }else{
+        print("\(number) ganjil")
+    }
+    number += 1
+}
+    
