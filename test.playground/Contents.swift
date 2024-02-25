636a452e80fe4cecd360e3b209e6689fc885775e
Zaki Farhan <>
2024-02-25 17:00:46 +0700

    adding myplayground to github

---
+++ test.playground/Contents.swift
@@ -0,0 +1,39 @@
+
+func printString () {
+    print("ini adalah fungsi mencetak string")
+}
+
+printString()
+
+
+func pertambahan(valueA: Int, valueB: Int) {
+    let hasil: Int = valueA + valueB
+    print("hasil dari pertambahan \(valueA) + \(valueB) = \(hasil)")
+}
+pertambahan(valueA: 20, valueB: 29)
+
+
+func perkalian(valueA: Int, valueB: Int) -> Int {
+    let hasil: Int = valueA * valueB
+    
+    return hasil
+}
+
+let hasilPengkalian: Int = perkalian(valueA: 200, valueB: 178)
+
+print(hasilPengkalian)
+
+func validation(value: Int) -> Bool {
+    if value % 2 == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+if validation(value: 23) {
+    print("genap")
+} else {
+    print("ganjil")
+}
+
